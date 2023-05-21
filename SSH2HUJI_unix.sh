70010f422e0dd6ba1e9e152bc140fbb0638def93
danielnachumdev <danielnachumdev@gmail.com>
2023-05-21 11:34:25 +0300

    updated to version 1.11

--- SSH2HUJI_unix.sh
+++ SSH2HUJI_unix.sh
@@ -12,7 +12,7 @@ if ! which ssh >/dev/null; then
 fi
 
 # Check version
-current_version=1.10
+current_version=1.11
 version_file=tmp
 echo "[$me] Checking version number..."
 curl --silent --output "$version_file" https://raw.githubusercontent.com/danielnachumdev/SSH2HUJI/main/version
